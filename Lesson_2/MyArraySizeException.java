9566f50c538387074372ed9e8eded69869c23e48
Zartas <stasik.1991@yandex.ru>
2022-08-21 11:28:00 +0300

    Lesson_2

---
+++ Lesson_2/MyArraySizeException.java
@@ -0,0 +1,7 @@
+package ru.geekbrains.qa.java2.lesson2.Home_Work;
+
+public class MyArraySizeException extends Exception {
+    public MyArraySizeException(String message) {
+        super(message);
+    }
+}
